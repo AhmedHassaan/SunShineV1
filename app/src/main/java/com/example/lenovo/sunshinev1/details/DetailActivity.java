a7bc854cf308293ed44324793fe398315328db93
AhmedHassaan <ahmedhassaan162@gmail.com>
2016-10-09 11:04:52 +0200

    Class format

--- app/src/main/java/com/example/lenovo/sunshinev1/details/DetailActivity.java
+++ app/src/main/java/com/example/lenovo/sunshinev1/details/DetailActivity.java
@@ -1,13 +1,9 @@
-package com.example.lenovo.sunshinev1.data;
+package com.example.lenovo.sunshinev1.details;
 
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.support.v7.app.ActionBarActivity;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
 
 import com.example.lenovo.sunshinev1.R;
 
@@ -47,20 +43,4 @@ public class DetailActivity extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-
-        public PlaceholderFragment() {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-
-            View rootView = inflater.inflate(R.layout.fragment_detail, container, false);
-            return rootView;
-        }
-    }
 }
---
+++ app/src/main/java/com/example/lenovo/sunshinev1/details/PlaceholderFragment.java
@@ -0,0 +1,35 @@
+package com.example.lenovo.sunshinev1.details;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.lenovo.sunshinev1.R;
+
+/**
+ * Created by Lenovo on 10/9/2016.
+ */
+
+
+public class PlaceholderFragment extends Fragment {
+
+    TextView text;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View rootView = inflater.inflate(R.layout.fragment_detail, container, false);
+        text = (TextView)rootView.findViewById(R.id.test);
+        Intent intent = getActivity().getIntent();
+        if(intent != null && intent.hasExtra(Intent.EXTRA_TEXT)){
+            String str = intent.getStringExtra(Intent.EXTRA_TEXT);
+            text.setText(str);
+        }
+        return rootView;
+    }
+}
